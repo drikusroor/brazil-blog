65bdb002a6bbbb346fe57c2714bcda1e1ddec20b
Drikus Roor <drikusroor@gmail.com>
2024-04-06 20:55:42 +0200

    refactor: Convert Django project to Wagtail project

---
+++ src/brazil_blog/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-d#qmoi$oy(bp7l!2x%rs(ltwu)9(s=%sweqb_mc9+6f24l#!9e"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
