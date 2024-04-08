a61295d89297467629567fc8898f982d87b04f7b
Drikus Roor <drikusroor@gmail.com>
2024-04-08 19:30:57 -0300

    feat: Add site settings

--- src/base/models.py
+++ src/base/models.py
@@ -10,11 +10,30 @@ from wagtail.contrib.settings.models import (
 
 @register_setting
 class NavigationSettings(BaseGenericSetting):
+
+    site_name = models.CharField(max_length=255, blank=True)
+    site_description = models.TextField(blank=True)
+    site_logo = models.ForeignKey(
+        "wagtailimages.Image",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+",
+    )
+
     twitter_url = models.URLField(verbose_name="Twitter URL", blank=True)
     github_url = models.URLField(verbose_name="GitHub URL", blank=True)
     mastodon_url = models.URLField(verbose_name="Mastodon URL", blank=True)
 
     panels = [
+        MultiFieldPanel(
+            [
+                FieldPanel("site_name"),
+                FieldPanel("site_description"),
+                FieldPanel("site_logo"),
+            ],
+            "Site settings",
+        ),
         MultiFieldPanel(
             [
                 FieldPanel("twitter_url"),
