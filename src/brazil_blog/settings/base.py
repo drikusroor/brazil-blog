28590de0dc98c75fefd2b5a9458f16fd32cf49cb
Drikus Roor <drikusroor@gmail.com>
2024-04-13 14:53:21 -0300

    Add Redis service to docker-compose.yml and update requirements.txt

--- src/brazil_blog/settings/base.py
+++ src/brazil_blog/settings/base.py
@@ -169,6 +169,16 @@ STORAGES = {
     },
 }
 
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://redis:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+
 
 # Wagtail settings
 
