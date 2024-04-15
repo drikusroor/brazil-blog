3bd72fde53b990cdbe0674d5f6e6d4d1a2917c89
Drikus Roor <drikusroor@gmail.com>
2024-04-15 10:50:48 -0300

    Update WAGTAILADMIN_BASE_URL and CSRF_TRUSTED_ORIGINS in base.py

--- src/brazil_blog/settings/base.py
+++ src/brazil_blog/settings/base.py
@@ -195,4 +195,6 @@ WAGTAILSEARCH_BACKENDS = {
 
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
-WAGTAILADMIN_BASE_URL = "http://example.com"
+WAGTAILADMIN_BASE_URL = "https://brazil.kokokoding.nl"
+
+CSRF_TRUSTED_ORIGINS=["brazil.kokokoding.nl"]
