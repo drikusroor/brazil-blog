c3c1e120b6ec247e356282b5b3b4200ca84e9c64
Drikus Roor <drikusroor@gmail.com>
2024-04-15 11:39:03 -0300

    Refactor Docker Compose files to remove redundant Redis service

--- src/brazil_blog/settings/base.py
+++ src/brazil_blog/settings/base.py
@@ -172,11 +172,8 @@ STORAGES = {
 
 CACHES = {
     "default": {
-        "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://redis:6379/1",
-        "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient",
-        }
+        "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
+        "LOCATION": "unique-snowflake"
     }
 }
 
