0f36506449f907edc61885871d60a70db426ac60
Drikus Roor <drikusroor@gmail.com>
2024-07-23 22:54:55 +0200

    feat: Update author display logic in blog templates
    Refactor the author display logic in the `blog_page.html` and `blog_index_page.html` templates to handle different scenarios. The code now checks for the presence of `page.author` and `page.owner` to display the correct author information. This change ensures consistent and accurate author attribution on the blog pages.

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,13 +1,18 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
 from django.utils import timezone
 from django.db import models
 from modelcluster.fields import ParentalKey
 
 from wagtail.models import Page, Orderable
 from wagtail.fields import RichTextField
-from wagtail.admin.panels import FieldPanel, InlinePanel
+from wagtail.admin.panels import FieldPanel, InlinePanel, ObjectList, TabbedInterface
+
+from wagtail.admin.forms import WagtailAdminPageForm
 
 from wagtail.search import index
 
+User = get_user_model()
 
 class BlogIndexPage(Page):
     intro = RichTextField(blank=True)
@@ -29,8 +34,19 @@ class BlogIndexPage(Page):
         FieldPanel('intro')
     ]
 
+class BlogPageForm(WagtailAdminPageForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if self.instance._state.adding:
+            self.initial['author'] = self.for_user.pk
 
 class BlogPage(Page):
+    author = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='blog_posts'
+    )
     date = models.DateTimeField("Post date", default=timezone.now)
     image = models.ForeignKey(
         "wagtailimages.Image",
@@ -49,12 +65,20 @@ class BlogPage(Page):
     ]
 
     content_panels = Page.content_panels + [
+        FieldPanel('author'),
         FieldPanel('date'),
         FieldPanel('image'),
         FieldPanel('intro'),
         FieldPanel('body'),
         InlinePanel('gallery_images', label="Gallery images"),
     ]
+
+    edit_handler = TabbedInterface([
+        ObjectList(content_panels, heading='Content'),
+        ObjectList(Page.promote_panels, heading='Promote'),
+    ])
+
+    base_form_class = BlogPageForm
 
 class Comment(models.Model):
     post = models.ForeignKey(BlogPage, on_delete=models.CASCADE, related_name='comments')
@@ -82,3 +106,40 @@ class BlogPageGalleryImage(Orderable):
         FieldPanel('image'),
         FieldPanel('caption'),
     ]
+
+
+class AuthorPage(Page):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='author_page')
+    bio = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('user'),
+        FieldPanel('bio'),
+    ]
+
+    def get_context(self, request):
+        context = super().get_context(request)
+        context['posts'] = self.user.blog_posts.live().order_by('-first_published_at')
+        return context
+
+    @classmethod
+    def can_create_at(cls, parent):
+        # You can only create one author page under the author index page
+        return super(AuthorPage, cls).can_create_at(parent) \
+               and not parent.get_children().type(cls)
+
+class AuthorIndexPage(Page):
+    intro = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('intro')
+    ]
+
+    def get_context(self, request):
+        context = super().get_context(request)
+        editor_group = Group.objects.get(name='Editors')
+        authors = editor_group.user_set.filter(blog_posts__isnull=False).distinct()
+        context['authors'] = authors
+        return context
+
+    subpage_types = ['AuthorPage']
