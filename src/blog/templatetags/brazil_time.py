a879cce08339446203874d93cc24d3c4849a971d
Drikus Roor <drikusroor@gmail.com>
2024-08-01 23:02:39 +0200

    config: Configure Ruff for VS Code

--- src/blog/templatetags/brazil_time.py
+++ src/blog/templatetags/brazil_time.py
@@ -5,23 +5,24 @@ import pytz
 
 register = template.Library()
 
+
 @register.simple_tag
 def display_brazil_time():
     # Define the timezone for São Paulo, Brazil
-    sao_paulo_tz = pytz.timezone('America/Sao_Paulo')
-    
+    sao_paulo_tz = pytz.timezone("America/Sao_Paulo")
+
     # Get the current time in UTC and convert it to São Paulo time
     sao_paulo_time = datetime.now(pytz.utc).astimezone(sao_paulo_tz)
-    
+
     # Format the time as a string
-    formatted_time = sao_paulo_time.strftime('%Y-%m-%d %H:%M')
-    
+    formatted_time = sao_paulo_time.strftime("%Y-%m-%d %H:%M")
+
     # Determine if it's day or night
     hour = sao_paulo_time.hour
     if 6 <= hour < 18:
-        emoji = '☀️'  # Sun emoji for day time
+        emoji = "☀️"  # Sun emoji for day time
     else:
-        emoji = '🌙'  # Moon emoji for night time
-    
+        emoji = "🌙"  # Moon emoji for night time
+
     # Return the formatted time with the appropriate emoji
     return format_html(f"🇧🇷 {formatted_time} {emoji}")
