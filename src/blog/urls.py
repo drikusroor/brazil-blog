37d1bf195079f818837f0fda0166400f02832500
Drikus Roor <drikusroor@gmail.com>
2024-09-21 22:49:51 +0200

    Added: Add "like" feature to blog posts and comments (#70)
    * feat: Add "like post" feature to blog posts
    
    * style: Refactor like button styling and functionality
    
    * style: Decrease margin-top of counter a bit
    
    * feat: Adjust likes information to blog overview grid
    
    * feat: Add likes functionality to comments
    
    * chore: Re-run tailwindcss

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CommentViewSet, BrazilTimeViewSet
+from .views import CommentViewSet, BrazilTimeViewSet, LikeViewSet, CommentLikeViewSet
 
 router = DefaultRouter()
-router.register(r'comments', CommentViewSet)
-router.register(r'brazil_time', BrazilTimeViewSet, basename='brazil-time')
+router.register(r"comments", CommentViewSet)
+router.register(r"brazil_time", BrazilTimeViewSet, basename="brazil-time")
+router.register(r"likes", LikeViewSet)
+router.register(r"comment-likes", CommentLikeViewSet, basename="comment-likes")
 
 
 urlpatterns = [
-    path('api/', include(router.urls)),
+    path("api/", include(router.urls)),
 ]
