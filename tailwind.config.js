bafaa38e3e02cec5c59618b60f9d4c9987ebcebb
Drikus Roor <drikusroor@gmail.com>
2024-09-22 13:10:23 +0200

    Refactor: Update tailwindcss's content paths to only scan _our_ source code and not that of the static folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,py}"],
+  content: ["./src/**/*.{html,js,py}", "!./src/static/**/*"],
   theme: {
     extend: {},
   },
