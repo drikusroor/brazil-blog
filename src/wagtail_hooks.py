01493baf0084dc339e13c56f58b3719a7fcf979b
Drikus Roor <drikusroor@gmail.com>
2024-10-04 20:48:22 +0200

    Refactor: Create notifications for published pages
    This commit adds functionality to create notifications for published pages. It retrieves all subscriptions for the page author and creates a notification for each subscription, indicating that a new post has been published. The notification includes the title of the page and a link to view it.

--- src/wagtail_hooks.py
+++ src/wagtail_hooks.py
@@ -1,9 +1,22 @@
 from wagtail import hooks
 
 from notifications.models import Notification
+from blog.models import Subscription
 
 
 # after publishing page
 @hooks.register("after_publish_page")
 def after_publish_page(request, page):
-    pass
+    # page author
+    author = page.author
+
+    # get all subscriptions for the page author
+    subscriptions = Subscription.objects.filter(author=author)
+
+    # create notification for each subscription
+    for subscription in subscriptions:
+        Notification.objects.create(
+            user=subscription.subscriber,
+            title=f"New post published: {page.title}",
+            url=page.get_url(),
+        )
