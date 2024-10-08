b193edc61c9e87c0fd9c4cce089cb24849fdeb87
Drikus Roor <drikusroor@gmail.com>
2024-10-08 14:53:49 +0200

    feat: Enable TLS by default for email settings

--- src/brazil_blog/settings/base.py
+++ src/brazil_blog/settings/base.py
@@ -210,3 +210,6 @@ EMAIL_HOST = os.environ.get("EMAIL_HOST")
 EMAIL_PORT = os.environ.get("EMAIL_PORT")
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+
+# enable TLS by default
+EMAIL_USE_TLS = True
