cbfbb38dbebb85c1667b8db1b6e8bfda2dfbce4d
Drikus Roor <drikusroor@gmail.com>
2024-10-09 11:14:18 +0200

    feat: Add actions to DrinkViewSet for counting drinks and total amount per type

--- src/drinks/views.py
+++ src/drinks/views.py
@@ -2,7 +2,9 @@ from rest_framework import viewsets
 from rest_framework import serializers
 from .models import DrinkConsumption
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
-from rest_framework.decorators import permission_classes
+from rest_framework.decorators import permission_classes, action
+from rest_framework.response import Response
+from django.db.models import Sum
 
 from blog.templatetags.user_tags import user_display_name
 
@@ -43,3 +45,30 @@ class DrinkViewSet(viewsets.ModelViewSet):
         context = super().get_serializer_context()
         context["request"] = self.request
         return context
+
+    # get drinks/count{?type=}
+    @action(detail=False, methods=["get"])
+    def get_drinks_count(self, request):
+        drinks_type = request.query_params.get("drink_type")
+
+        if drinks_type:
+            # count times amount field
+            count = DrinkConsumption.objects.filter(drink_type=drinks_type).aggregate(
+                count=Sum("amount")
+            )
+
+            return Response({"count": count})
+
+        count = DrinkConsumption.objects.aggregate(count=Sum("amount"))
+        return Response({"count": count})
+
+    # get total amount per type
+    @action(detail=False, methods=["get"])
+    def get_total_amount_per_type(self, request):
+        total_amount_per_type = (
+            DrinkConsumption.objects.values("drink_type")
+            .annotate(total_amount=Sum("amount"))
+            .order_by("drink_type")
+        )
+
+        return Response(total_amount_per_type)
